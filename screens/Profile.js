475c52ce8fbfb65c3ddd2a1953893a0008db10fb
Sodabeh Sherzad <42426114+SodabehSherzad@users.noreply.github.com>
2021-02-27 18:07:01 +0430

    Added Profile.js to show more detail about contact

--- screens/Profile.js
+++ screens/Profile.js
@@ -5,15 +5,17 @@ import colors from '../utils/colors.js'
 import ContactThumnail from '../components/ContactThumnail'
 import DetailsListItem from '../components/DetailsListItem'
 
-export default function Profile({name, phone, email}){
+export default function Profile(props){
+    const { route } = props
+    const { item } = route.params 
     return (
         <View style={styles.container}>
             <View style={styles.thumnailSection}>
-                <ContactThumnail name={name} phone={phone}/>
+                <ContactThumnail name={item.name} phone={item.phone}/>
             </View>
             <View style={styles.detailSection}>
-                <DetailsListItem icon="email" title="Email" subTitle={email}/>
-                <DetailsListItem icon="phone" title="work" subTitle={phone}/>     
+                <DetailsListItem icon="email" title="Email" subTitle={item.email}/>
+                <DetailsListItem icon="phone" title="work" subTitle={item.phone}/>     
             </View>
         </View>
     )
