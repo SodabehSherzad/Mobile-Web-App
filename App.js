3a0c5352312f8dfa472cb1626f0202142eb7e122
Sodabeh Sherzad <42426114+SodabehSherzad@users.noreply.github.com>
2021-02-27 18:05:29 +0430

    Used database for having dynamic pages
    Database => SQLite
    CRUD => (Create, Read, Update, Delete)

--- App.js
+++ App.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import { MaterialIcons } from '@expo/vector-icons'
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+const Stack = createStackNavigator();
+import Contact from './screens/Contacts.js'
 import Profile from './screens/Profile.js'
+import CreateContact from './screens/CreateContacts.js'
+import SearchScreen from './screens/SearchScreen.js'
+
+import * as SQLite from 'expo-sqlite' 
+
+const db = SQLite.openDatabase('contacts.db')
 
 export default function App() {
+  useEffect(() => {
+    db.transaction( tx => {
+      tx.executeSql('create table contacts(id integer primary key autoincrement, name text, phone text, email text);', [], ()=>console.log("table created successfully"))
+    })
+  })
   return (
-    <View style={styles.container}>
-      <Profile name="sodabeh" phone="0777888999" email="sodabeh@gmail.com"/>
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator>
+        <Stack.Screen name="Contacts" component={Contact} 
+          options={({navigation}) => ({
+            headerRight: () => (
+              <TouchableOpacity style={{paddingRight: 20}} onPress={() => navigation.navigate("Search")}>
+                <Text>
+                  <MaterialIcons name="search" size={24} color="black" />
+                </Text>
+              </TouchableOpacity>
+            )
+          })}
+        />
+        <Stack.Screen name="CreateContact" component={CreateContact}/>
+        <Stack.Screen name="Profile" component={Profile}/>
+        <Stack.Screen name="Search" component={SearchScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: "#f8f4f4"
   },
 });
